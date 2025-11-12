e69196f937dcedddf47063fccf8ef703c214bc95
rochellearis <arisnrochelle@gmail.com>
2025-11-12 16:48:18 +0700

    Tutorial 8: Flutter Networking, Authentication, and Integration

--- lib/widgets/left_drawer.dart
+++ lib/widgets/left_drawer.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:football_news/screens/menu.dart';
 import 'package:football_news/screens/newslist_form.dart'; // import NewsFormPage
+import 'package:football_news/screens/news_entry_list.dart';
 
 class LeftDrawer extends StatelessWidget {
   const LeftDrawer({super.key});
@@ -57,6 +58,17 @@ class LeftDrawer extends StatelessWidget {
             onTap: () {
               // Navigasi ke halaman NewsFormPage
               Navigator.push(context, MaterialPageRoute(builder: (context) => const NewsFormPage()));
+            },
+          ),
+          ListTile(
+            leading: const Icon(Icons.add_reaction_rounded),
+            title: const Text('News List'),
+            onTap: () {
+                // Route to news list page
+                Navigator.push(
+                    context,
+                    MaterialPageRoute(builder: (context) => const NewsEntryListPage()),
+                );
             },
           ),
         ],
